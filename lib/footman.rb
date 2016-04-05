17e2fec593770fb67595ba9f308b69d247eb6b13
vinoscript <wstikk@gmail.com>
2016-04-05 20:24:13 +0000

    Passed rspec test 1

--- lib/footman.rb
+++ lib/footman.rb
@@ -2,9 +2,13 @@
 
 class Footman
 
+  attr_accessor :health_points, :attack_power
+
   def initialize
     # Need to default the 2 instance variables here
     # Also also give code outside this class access to these variables (via attr_reader, attr_writer or attr_accessor)
+    @health_points = 60
+    @attack_power = 10
   end
 
 end
